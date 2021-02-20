9d80c94ca00722f9feaa4097479457c4520206e7
Samuel <samuel.andrade@fatec.sp.gov.br>
2021-02-20 13:50:16 -0300

    Service para Employee

--- src/main/java/sam/employeemanager/repository/EmployeeRepository.java
+++ src/main/java/sam/employeemanager/repository/EmployeeRepository.java
@@ -1,8 +1,13 @@
 package sam.employeemanager.repository;
 
-import sam.employeemanager.model.Employee;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+import sam.employeemanager.model.Employee;
 
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
---
+++ src/main/java/sam/employeemanager/service/EmployeeService.java
@@ -0,0 +1,40 @@
+package sam.employeemanager.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sam.employeemanager.exception.UserNotFoundException;
+import sam.employeemanager.model.Employee;
+import sam.employeemanager.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	EmployeeRepository repository;
+
+	public Employee addEmployee(Employee employee) {
+		employee.setEmployeeCode(UUID.randomUUID().toString());
+		return repository.save(employee);
+	}
+	
+	public List<Employee> findAllEmployees() {
+		return repository.findAll();
+	}
+	
+	public Employee updateEmployee(Employee employee) {
+		return repository.save(employee);
+	}
+	
+	public void deleteEmployee(Long id) {
+		repository.deleteEmployeeById(id);
+	}
+	
+	public Employee findEmployeeById(Long id) {
+        return repository.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+}
